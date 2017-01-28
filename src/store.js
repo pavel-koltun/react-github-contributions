2fb96ac93b4c9db4d898b62412ce6b5c3fee331a
koltun.pavel <koltun.pavel@gmail.com>
2017-01-28 17:54:11 +0300

    Sync store state to localstorage

--- src/store.js
+++ src/store.js
@@ -1,5 +1,22 @@
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
+import persistState from 'redux-localstorage';
+import moment from 'moment';
 
 import reducers from './reducers';
 
-export default createStore(reducers);
+const config = {
+  key: 'react-github-contributions',
+  deserialize: (persistedState) => {
+    let state = JSON.parse(persistedState);
+
+    if (state && state.dates) {
+      state = { ...state, dates: state.dates.map(d => moment(d)) };
+    }
+
+    return state;
+  },
+};
+
+const enhancer = compose(persistState(null, config));
+
+export default createStore(reducers, enhancer);
